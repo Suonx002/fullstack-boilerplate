e8128d385c985b740950309f07650e7dfd4839c6
Vuthy <hello@vuthysuon.com>
2021-09-14 21:35:44 -0500

    adding users data for testing

---
+++ database/mockup-data/users-data.js
@@ -0,0 +1,965 @@
+module.exports = [
+	{
+		email: 'dpesselt5o@omniture.com',
+		firstName: 'Darcy',
+		lastName: 'Pesselt',
+		password: '123456',
+	},
+	{
+		email: 'mcowden5p@cmu.edu',
+		firstName: 'Martha',
+		lastName: 'Cowden',
+		password: '123456',
+	},
+	{
+		email: 'igyer5q@bizjournals.com',
+		firstName: 'Isobel',
+		lastName: 'Gyer',
+
+		password: '123456',
+	},
+	{
+		email: 'rdimitrijevic5r@slideshare.net',
+		firstName: 'Robina',
+		lastName: 'Dimitrijevic',
+		password: '123456',
+	},
+	{
+		email: 'vmalt5s@xinhuanet.com',
+		firstName: 'Vivia',
+		lastName: 'Malt',
+		password: '123456',
+	},
+	{
+		email: 'cocuolahan5t@myspace.com',
+		firstName: 'Chandal',
+		lastName: "O' Cuolahan",
+
+		password: '123456',
+	},
+	{
+		email: 'ldeangelis5u@ehow.com',
+		firstName: 'Leann',
+		lastName: 'De Angelis',
+		password: '123456',
+	},
+	{
+		email: 'trostern5v@bing.com',
+		firstName: 'Therese',
+		lastName: 'Rostern',
+		password: '123456',
+	},
+	{
+		email: 'tdurgan5w@jimdo.com',
+		firstName: 'Thane',
+		lastName: 'Durgan',
+
+		password: '123456',
+	},
+	{
+		email: 'nemmins5x@jugem.jp',
+		firstName: 'Nels',
+		lastName: 'Emmins',
+
+		password: '123456',
+	},
+	{
+		email: 'gjosiah5y@cmu.edu',
+		firstName: 'Gus',
+		lastName: 'Josiah',
+		password: '123456',
+	},
+	{
+		email: 'rchaundy5z@twitter.com',
+		firstName: 'Rhetta',
+		lastName: 'Chaundy',
+		password: '123456',
+	},
+	{
+		email: 'rmiranda60@e-recht24.de',
+		firstName: 'Rhody',
+		lastName: 'Miranda',
+
+		password: '123456',
+	},
+	{
+		email: 'pcrosson61@google.pl',
+		firstName: 'Pat',
+		lastName: 'Crosson',
+
+		password: '123456',
+	},
+	{
+		email: 'kdemetz62@furl.net',
+		firstName: 'Kari',
+		lastName: 'De Metz',
+
+		password: '123456',
+	},
+	{
+		email: 'dgerold63@hubpages.com',
+		firstName: 'Dionysus',
+		lastName: 'Gerold',
+
+		password: '123456',
+	},
+	{
+		email: 'uzapatero64@foxnews.com',
+		firstName: 'Urbanus',
+		lastName: 'Zapatero',
+
+		password: '123456',
+	},
+	{
+		email: 'vturpey65@mac.com',
+		firstName: 'Vanessa',
+		lastName: 'Turpey',
+
+		password: '123456',
+	},
+	{
+		email: 'kebertz66@odnoklassniki.ru',
+		firstName: 'Kary',
+		lastName: 'Ebertz',
+		password: '123456',
+	},
+	{
+		email: 'afarrears67@discovery.com',
+		firstName: 'Andonis',
+		lastName: 'Farrears',
+
+		password: '123456',
+	},
+	{
+		email: 'qvalek68@wisc.edu',
+		firstName: 'Quinta',
+		lastName: 'Valek',
+		password: '123456',
+	},
+	{
+		email: 'mfratson69@rambler.ru',
+		firstName: 'Massimo',
+		lastName: 'Fratson',
+		password: '123456',
+	},
+	{
+		email: 'sklousner6a@uol.com.br',
+		firstName: 'Skelly',
+		lastName: 'Klousner',
+
+		password: '123456',
+	},
+	{
+		email: 'hparradye6b@xrea.com',
+		firstName: 'Honor',
+		lastName: 'Parradye',
+
+		password: '123456',
+	},
+	{
+		email: 'jconew6c@vkontakte.ru',
+		firstName: 'Janna',
+		lastName: 'Conew',
+		password: '123456',
+	},
+	{
+		email: 'jmatzl6d@businessweek.com',
+		firstName: 'Jessie',
+		lastName: 'Matzl',
+		password: '123456',
+	},
+	{
+		email: 'cpitkeathley6e@forbes.com',
+		firstName: 'Chelsie',
+		lastName: 'Pitkeathley',
+		password: '123456',
+	},
+	{
+		email: 'obrauninger6f@ucsd.edu',
+		firstName: 'Ozzy',
+		lastName: 'Brauninger',
+
+		password: '123456',
+	},
+	{
+		email: 'binkster6g@rambler.ru',
+		firstName: 'Briano',
+		lastName: 'Inkster',
+		password: '123456',
+	},
+	{
+		email: 'ipeete6h@vk.com',
+		firstName: 'Ivor',
+		lastName: 'Peete',
+
+		password: '123456',
+	},
+	{
+		email: 'mtheuss6i@ebay.com',
+		firstName: 'Melody',
+		lastName: 'Theuss',
+		password: '123456',
+	},
+	{
+		email: 'wpleasaunce6j@theatlantic.com',
+		firstName: 'Willyt',
+		lastName: 'Pleasaunce',
+
+		password: '123456',
+	},
+	{
+		email: 'rshovell6k@storify.com',
+		firstName: 'Roze',
+		lastName: 'Shovell',
+
+		password: '123456',
+	},
+	{
+		email: 'sroberto6l@nifty.com',
+		firstName: 'Sofia',
+		lastName: 'Roberto',
+		password: '123456',
+	},
+	{
+		email: 'caarons6m@bizjournals.com',
+		firstName: 'Ciel',
+		lastName: 'Aarons',
+		password: '123456',
+	},
+	{
+		email: 'gelmore6n@about.com',
+		firstName: 'Gilbertine',
+		lastName: 'Elmore',
+
+		password: '123456',
+	},
+	{
+		email: 'wmerrilees6o@icq.com',
+		firstName: 'Wynne',
+		lastName: 'Merrilees',
+
+		password: '123456',
+	},
+	{
+		email: 'amaas6p@miibeian.gov.cn',
+		firstName: 'Amelia',
+		lastName: 'Maas',
+		password: '123456',
+	},
+	{
+		email: 'fdurston6q@parallels.com',
+		firstName: 'Frannie',
+		lastName: 'Durston',
+		password: '123456',
+	},
+	{
+		email: 'cantonioni6r@cyberchimps.com',
+		firstName: 'Coretta',
+		lastName: 'Antonioni',
+
+		password: '123456',
+	},
+	{
+		email: 'vmarjot6s@infoseek.co.jp',
+		firstName: 'Viva',
+		lastName: 'Marjot',
+
+		password: '123456',
+	},
+	{
+		email: 'motton6t@statcounter.com',
+		firstName: 'Masha',
+		lastName: 'Otton',
+
+		password: '123456',
+	},
+	{
+		email: 'ncallaby6u@cafepress.com',
+		firstName: 'Nyssa',
+		lastName: 'Callaby',
+		password: '123456',
+	},
+	{
+		email: 'pmacnockater6v@unicef.org',
+		firstName: 'Perren',
+		lastName: 'MacNockater',
+
+		password: '123456',
+	},
+	{
+		email: 'rfarrance6w@dion.ne.jp',
+		firstName: 'Ransom',
+		lastName: 'Farrance',
+
+		password: '123456',
+	},
+	{
+		email: 'aglusby6x@seattletimes.com',
+		firstName: 'Aimil',
+		lastName: 'Glusby',
+
+		password: '123456',
+	},
+	{
+		email: 'zwelling6y@dedecms.com',
+		firstName: 'Zared',
+		lastName: 'Welling',
+
+		password: '123456',
+	},
+	{
+		email: 'aguinan6z@sciencedaily.com',
+		firstName: 'Annecorinne',
+		lastName: 'Guinan',
+
+		password: '123456',
+	},
+	{
+		email: 'vloines70@sohu.com',
+		firstName: 'Vikki',
+		lastName: 'Loines',
+
+		password: '123456',
+	},
+	{
+		email: 'istorre71@hibu.com',
+		firstName: 'Iain',
+		lastName: 'Storre',
+
+		password: '123456',
+	},
+	{
+		email: 'cswan72@sourceforge.net',
+		firstName: 'Craig',
+		lastName: 'Swan',
+
+		password: '123456',
+	},
+	{
+		email: 'dwynrahame73@vk.com',
+		firstName: 'Dorena',
+		lastName: 'Wynrahame',
+
+		password: '123456',
+	},
+	{
+		email: 'ktolhurst74@oakley.com',
+		firstName: 'Kissiah',
+		lastName: 'Tolhurst',
+
+		password: '123456',
+	},
+	{
+		email: 'eadamsky75@patch.com',
+		firstName: 'Enrica',
+		lastName: 'Adamsky',
+		password: '123456',
+	},
+	{
+		email: 'mchafer76@rambler.ru',
+		firstName: 'Marty',
+		lastName: 'Chafer',
+		password: '123456',
+	},
+	{
+		email: 'klavalde77@si.edu',
+		firstName: 'Kit',
+		lastName: 'Lavalde',
+
+		password: '123456',
+	},
+	{
+		email: 'ameineking78@t-online.de',
+		firstName: 'Arie',
+		lastName: 'Meineking',
+
+		password: '123456',
+	},
+	{
+		email: 'cbolger79@ycombinator.com',
+		firstName: 'Cliff',
+		lastName: 'Bolger',
+		password: '123456',
+	},
+	{
+		email: 'qocarney7a@dell.com',
+		firstName: 'Quinton',
+		lastName: "O'Carney",
+
+		password: '123456',
+	},
+	{
+		email: 'sciottoi7b@amazon.co.jp',
+		firstName: 'Stacia',
+		lastName: 'Ciottoi',
+		password: '123456',
+	},
+	{
+		email: 'glisle7c@cisco.com',
+		firstName: 'Gibb',
+		lastName: 'Lisle',
+
+		password: '123456',
+	},
+	{
+		email: 'zberrill7d@smugmug.com',
+		firstName: 'Zane',
+		lastName: 'Berrill',
+
+		password: '123456',
+	},
+	{
+		email: 'dguerrier7e@cbc.ca',
+		firstName: 'Delcina',
+		lastName: 'Guerrier',
+
+		password: '123456',
+	},
+	{
+		email: 'stuxwell7f@thetimes.co.uk',
+		firstName: 'Sabina',
+		lastName: 'Tuxwell',
+
+		password: '123456',
+	},
+	{
+		email: 'asherry7g@google.com.hk',
+		firstName: 'Ailis',
+		lastName: 'Sherry',
+
+		password: '123456',
+	},
+	{
+		email: 'gjanuszewicz7h@sourceforge.net',
+		firstName: 'Genni',
+		lastName: 'Januszewicz',
+
+		password: '123456',
+	},
+	{
+		email: 'lgotch7i@scientificamerican.com',
+		firstName: 'Lacey',
+		lastName: 'Gotch',
+		password: '123456',
+	},
+	{
+		email: 'ckhristoforov7j@furl.net',
+		firstName: 'Carlene',
+		lastName: 'Khristoforov',
+		password: '123456',
+	},
+	{
+		email: 'nsheirlaw7k@ocn.ne.jp',
+		firstName: 'Nicolai',
+		lastName: 'Sheirlaw',
+
+		password: '123456',
+	},
+	{
+		email: 'smoffett7l@usda.gov',
+		firstName: 'Sascha',
+		lastName: 'Moffett',
+
+		password: '123456',
+	},
+	{
+		email: 'dscoular7m@sciencedirect.com',
+		firstName: 'Derby',
+		lastName: 'Scoular',
+
+		password: '123456',
+	},
+	{
+		email: 'ssmurthwaite7n@delicious.com',
+		firstName: 'Stacee',
+		lastName: 'Smurthwaite',
+
+		password: '123456',
+	},
+	{
+		email: 'dtodd7o@bloglovin.com',
+		firstName: 'Daffy',
+		lastName: 'Todd',
+
+		password: '123456',
+	},
+	{
+		email: 'wgrob7p@skype.com',
+		firstName: 'Wilmette',
+		lastName: 'Grob',
+
+		password: '123456',
+	},
+	{
+		email: 'klaugheran7q@umn.edu',
+		firstName: 'Kimberley',
+		lastName: 'Laugheran',
+
+		password: '123456',
+	},
+	{
+		email: 'blehrer7r@eepurl.com',
+		firstName: 'Bertie',
+		lastName: 'Lehrer',
+		password: '123456',
+	},
+	{
+		email: 'blumox7s@xing.com',
+		firstName: 'Beauregard',
+		lastName: 'Lumox',
+		password: '123456',
+	},
+	{
+		email: 'jbaxill7t@redcross.org',
+		firstName: 'Jayme',
+		lastName: 'Baxill',
+		password: '123456',
+	},
+	{
+		email: 'owillmot7u@nps.gov',
+		firstName: 'Orelee',
+		lastName: 'Willmot',
+		password: '123456',
+	},
+	{
+		email: 'abeddingham7v@hp.com',
+		firstName: 'Abrahan',
+		lastName: 'Beddingham',
+
+		password: '123456',
+	},
+	{
+		email: 'fbreazeall7w@ted.com',
+		firstName: 'Ferrel',
+		lastName: 'Breazeall',
+		password: '123456',
+	},
+	{
+		email: 'zhealeas7x@yandex.ru',
+		firstName: 'Zach',
+		lastName: 'Healeas',
+		password: '123456',
+	},
+	{
+		email: 'mjacombs7y@angelfire.com',
+		firstName: 'Marie',
+		lastName: 'Jacombs',
+		password: '123456',
+	},
+	{
+		email: 'gpaulmann7z@about.me',
+		firstName: 'Gerhard',
+		lastName: 'Paulmann',
+
+		password: '123456',
+	},
+	{
+		email: 'cygoe80@barnesandnoble.com',
+		firstName: 'Carolan',
+		lastName: 'Ygoe',
+
+		password: '123456',
+	},
+	{
+		email: 'htreamayne81@etsy.com',
+		firstName: 'Humberto',
+		lastName: 'Treamayne',
+		password: '123456',
+	},
+	{
+		email: 'wpettiford82@mlb.com',
+		firstName: 'William',
+		lastName: 'Pettiford',
+		password: '123456',
+	},
+	{
+		email: 'ahaxby83@phoca.cz',
+		firstName: 'Anna-diane',
+		lastName: 'Haxby',
+
+		password: '123456',
+	},
+	{
+		email: 'mbabon84@cbslocal.com',
+		firstName: 'Margo',
+		lastName: 'Babon',
+
+		password: '123456',
+	},
+	{
+		email: 'rshervil85@vinaora.com',
+		firstName: 'Raina',
+		lastName: 'Shervil',
+
+		password: '123456',
+	},
+	{
+		email: 'lansill86@webmd.com',
+		firstName: 'Lindsay',
+		lastName: 'Ansill',
+		password: '123456',
+	},
+	{
+		email: 'rhodgins87@usatoday.com',
+		firstName: 'Rossy',
+		lastName: 'Hodgins',
+
+		password: '123456',
+	},
+	{
+		email: 'mmarcome88@ft.com',
+		firstName: 'Malia',
+		lastName: 'Marcome',
+		password: '123456',
+	},
+	{
+		email: 'mgillbey89@fotki.com',
+		firstName: 'Malvin',
+		lastName: 'Gillbey',
+
+		password: '123456',
+	},
+	{
+		email: 'kmetheringham8a@seattletimes.com',
+		firstName: 'Kynthia',
+		lastName: 'Metheringham',
+		password: '123456',
+	},
+	{
+		email: 'tduquesnay8b@google.com.au',
+		firstName: 'Timmy',
+		lastName: 'Duquesnay',
+		password: '123456',
+	},
+	{
+		email: 'epointon8c@mapy.cz',
+		firstName: 'Edward',
+		lastName: 'Pointon',
+		password: '123456',
+	},
+	{
+		email: 'moshields8d@google.it',
+		firstName: 'Minnaminnie',
+		lastName: "O'Shields",
+		password: '123456',
+	},
+	{
+		email: 'ebridgeland8e@harvard.edu',
+		firstName: 'Emilie',
+		lastName: 'Bridgeland',
+
+		password: '123456',
+	},
+	{
+		email: 'cparzis8f@hhs.gov',
+		firstName: 'Crista',
+		lastName: 'Parzis',
+
+		password: '123456',
+	},
+	{
+		email: 'iducastel8g@cocolog-nifty.com',
+		firstName: 'Imojean',
+		lastName: 'Ducastel',
+
+		password: '123456',
+	},
+	{
+		email: 'rrayment8h@wsj.com',
+		firstName: 'Rubetta',
+		lastName: 'Rayment',
+
+		password: '123456',
+	},
+	{
+		email: 'sperrygo8i@ted.com',
+		firstName: 'Salome',
+		lastName: 'Perrygo',
+
+		password: '123456',
+	},
+	{
+		email: 'gherkess8j@topsy.com',
+		firstName: 'Gavrielle',
+		lastName: 'Herkess',
+		password: '123456',
+	},
+	{
+		email: 'ztrussell8k@wsj.com',
+		firstName: 'Zabrina',
+		lastName: 'Trussell',
+
+		password: '123456',
+	},
+	{
+		email: 'rdrayn8l@hubpages.com',
+		firstName: 'Raine',
+		lastName: 'Drayn',
+
+		password: '123456',
+	},
+	{
+		email: 'xhuntall8m@vk.com',
+		firstName: 'Xerxes',
+		lastName: 'Huntall',
+
+		password: '123456',
+	},
+	{
+		email: 'dcorzon8n@goodreads.com',
+		firstName: 'Dalia',
+		lastName: 'Corzon',
+		password: '123456',
+	},
+	{
+		email: 'eyoutead8o@washingtonpost.com',
+		firstName: 'Euphemia',
+		lastName: 'Youtead',
+		password: '123456',
+	},
+	{
+		email: 'imasse8p@people.com.cn',
+		firstName: 'Inigo',
+		lastName: 'Masse',
+
+		password: '123456',
+	},
+	{
+		email: 'maxe8q@un.org',
+		firstName: 'Marcelle',
+		lastName: 'Axe',
+
+		password: '123456',
+	},
+	{
+		email: 'jkitchingman8r@chron.com',
+		firstName: 'Johnath',
+		lastName: 'Kitchingman',
+
+		password: '123456',
+	},
+	{
+		email: 'dgerrietz8s@xing.com',
+		firstName: 'Desi',
+		lastName: 'Gerrietz',
+
+		password: '123456',
+	},
+	{
+		email: 'csilby8t@baidu.com',
+		firstName: 'Constantine',
+		lastName: 'Silby',
+
+		password: '123456',
+	},
+	{
+		email: 'tklimmek8u@imageshack.us',
+		firstName: 'Thayne',
+		lastName: 'Klimmek',
+		password: '123456',
+	},
+	{
+		email: 'mkingsland8v@fema.gov',
+		firstName: 'Marje',
+		lastName: 'Kingsland',
+		password: '123456',
+	},
+	{
+		email: 'pgilham8w@cdc.gov',
+		firstName: 'Phedra',
+		lastName: 'Gilham',
+
+		password: '123456',
+	},
+	{
+		email: 'areddish8x@arstechnica.com',
+		firstName: 'Alessandra',
+		lastName: 'Reddish',
+
+		password: '123456',
+	},
+	{
+		email: 'rgouda8y@earthlink.net',
+		firstName: 'Rubie',
+		lastName: 'Gouda',
+		password: '123456',
+	},
+	{
+		email: 'mgreenalf8z@whitehouse.gov',
+		firstName: 'Mira',
+		lastName: 'Greenalf',
+
+		password: '123456',
+	},
+	{
+		email: 'wwalentynowicz90@yale.edu',
+		firstName: 'Westbrooke',
+		lastName: 'Walentynowicz',
+
+		password: '123456',
+	},
+	{
+		email: 'ttighe91@youtu.be',
+		firstName: 'Thomasa',
+		lastName: 'Tighe',
+		password: '123456',
+	},
+	{
+		email: 'sdipple92@pen.io',
+		firstName: 'Silvio',
+		lastName: 'Dipple',
+
+		password: '123456',
+	},
+	{
+		email: 'acansdill93@ted.com',
+		firstName: 'Aristotle',
+		lastName: 'Cansdill',
+		password: '123456',
+	},
+	{
+		email: 'vhawkswell94@ameblo.jp',
+		firstName: 'Vyky',
+		lastName: 'Hawkswell',
+
+		password: '123456',
+	},
+	{
+		email: 'kmacgorman95@github.io',
+		firstName: 'Kerrill',
+		lastName: 'MacGorman',
+
+		password: '123456',
+	},
+	{
+		email: 'mmatthiae96@umich.edu',
+		firstName: 'Matthias',
+		lastName: 'Matthiae',
+
+		password: '123456',
+	},
+	{
+		email: 'fdikes97@tamu.edu',
+		firstName: 'Fern',
+		lastName: 'Dikes',
+		password: '123456',
+	},
+	{
+		email: 'mfitzer98@xinhuanet.com',
+		firstName: 'Mirna',
+		lastName: 'Fitzer',
+
+		password: '123456',
+	},
+	{
+		email: 'gmeffin99@slashdot.org',
+		firstName: 'Ginnie',
+		lastName: 'Meffin',
+
+		password: '123456',
+	},
+	{
+		email: 'chartly9a@dedecms.com',
+		firstName: 'Cleveland',
+		lastName: 'Hartly',
+
+		password: '123456',
+	},
+	{
+		email: 'cdrezzer9b@comcast.net',
+		firstName: 'Christalle',
+		lastName: 'Drezzer',
+
+		password: '123456',
+	},
+	{
+		email: 'adeetch9c@posterous.com',
+		firstName: 'Alfons',
+		lastName: 'Deetch',
+
+		password: '123456',
+	},
+	{
+		email: 'rwitherington9d@blogger.com',
+		firstName: 'Regine',
+		lastName: 'Witherington',
+		password: '123456',
+	},
+	{
+		email: 'zcarlsen9e@cpanel.net',
+		firstName: 'Zoe',
+		lastName: 'Carlsen',
+
+		password: '123456',
+	},
+	{
+		email: 'eshearsby9f@a8.net',
+		firstName: 'Elsinore',
+		lastName: 'Shearsby',
+
+		password: '123456',
+	},
+	{
+		email: 'gdockrell9g@columbia.edu',
+		firstName: 'Grace',
+		lastName: 'Dockrell',
+		password: '123456',
+	},
+	{
+		email: 'nizkovicz9h@friendfeed.com',
+		firstName: 'Norene',
+		lastName: 'Izkovicz',
+		password: '123456',
+	},
+	{
+		email: 'jleachman9i@yahoo.co.jp',
+		firstName: 'Jerald',
+		lastName: 'Leachman',
+		password: '123456',
+	},
+	{
+		email: 'ggilardengo9j@jigsy.com',
+		firstName: 'Gunar',
+		lastName: 'Gilardengo',
+
+		password: '123456',
+	},
+	{
+		email: 'cdelmonte9k@si.edu',
+		firstName: 'Claudius',
+		lastName: 'Del Monte',
+
+		password: '123456',
+	},
+	{
+		email: 'cesmonde9l@twitter.com',
+		firstName: 'Cobby',
+		lastName: 'Esmonde',
+
+		password: '123456',
+	},
+	{
+		email: 'fmatskiv9m@chicagotribune.com',
+		firstName: 'Fleur',
+		lastName: 'Matskiv',
+		password: '123456',
+	},
+	{
+		email: 'cclyde9n@businessweek.com',
+		firstName: 'Cherish',
+		lastName: 'Clyde',
+		password: '123456',
+	},
+	{
+		email: 'npedel9o@ucla.edu',
+		firstName: 'Nappie',
+		lastName: 'Pedel',
+		password: '123456',
+	},
+	{
+		email: 'boddy9p@microsoft.com',
+		firstName: 'Brittani',
+		lastName: 'Oddy',
+		password: '123456',
+	},
+];
---
+++ database/seeds/0001_users.js
@@ -0,0 +1,51 @@
+const { v4: uuidv4 } = require('uuid');
+
+const bcryptMethods = require('../../utils/bcryptMethods');
+
+const tableNames = require('../constants/tableNames');
+
+const usersData = require('../mockup-data/users-data');
+
+exports.seed = async (knex) => {
+	try {
+		// clear all data
+		await knex(tableNames.users).del();
+
+		let dbUserList = [];
+
+		let index = 1;
+
+		console.log('This function may takes a few minutes');
+		for (const user of usersData) {
+			const id = uuidv4();
+			const { firstName, lastName, email, password } = user;
+
+			const newPassword = await bcryptMethods.hashPassword(password);
+
+			dbUserList.push({
+				firstName,
+				lastName,
+				email,
+				password: newPassword,
+				role: 'user',
+				id,
+				passwordChangedAt: new Date().toISOString(),
+				forgotPasswordToken: '',
+				forgotPasswordTokenExpires: new Date().toISOString(),
+				createdAt: new Date().toISOString(),
+				updatedAt: new Date().toISOString(),
+			});
+
+			console.log(`Complete ${index} out of ${usersData.length}`);
+
+			index++;
+		}
+
+		// insert new data
+		await knex(tableNames.users).insert(dbUserList);
+
+		console.log('Successfully inserted all users');
+	} catch (err) {
+		console.log('Something went wrong with seeding users');
+	}
+};
