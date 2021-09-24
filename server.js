3363efc7f80b7377606578348107b146adbd6b53
Vuthy <hello@vuthysuon.com>
2021-09-23 21:39:12 -0500

    adding react static files for serve

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ if (process.env.NODE_ENV !== 'production') {
 	app.use(morgan('tiny'));
 }
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'build')));
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
+}
+
 app.get('/', async (req, res) => {
 	// (from, to, subject, html)
 	// await sendEmail(
